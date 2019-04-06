e6ec6e7942bb9cbdca7b1dc17d463ab719db0783
csmasoudi <csmasoudi@gmail.com>
2019-04-06 17:39:18 +0430

    make plugin information

---
+++ imap.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name:       Imap
+ * Plugin URI:        wpro.ir
+ * Description:       The Imap plugin is a great plugin for Iranian companies that they can display their agencies on a beautiful SVG map.
+ * Version:           1.0.0
+ * Author:            Masoudi
+ * Author URI:        wpro.ir
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       imap
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}

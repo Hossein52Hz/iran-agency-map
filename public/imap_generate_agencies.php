bcc2f4d10a4d5e65054d7e51fb8a0e4e4ffb251a
csmasoudi <csmasoudi@gmail.com>
2019-04-10 14:32:30 +0430

    add function for generate area

---
+++ public/imap_generate_agencies.php
@@ -0,0 +1,33 @@
+<?php
+//generate province's name and their agencies 
+function imap_generate_areas()
+{
+    global $wpdb;
+    $province_result = $wpdb->get_results( ' SELECT * FROM wp_imap_province ' );
+    $agencies_result = $wpdb->get_results( ' SELECT * FROM wp_imap ' );
+    echo 'areas: {';
+    foreach ( $province_result AS $row1 ) 
+    {
+                echo '"' . $row1->province_en_name . '": {';
+                    
+                    if ( $row1->province_en_name == 'caspian' || $row1->province_en_name == 'persian-gulf' ) {
+                        // sea and gulf default color
+                        echo 'attrs: {fill: "rgb(108, 174, 216)"},';
+                    }                    
+
+                   echo 'tooltip: {content: ' . '"' . $row1->province_fa_name . '"},';
+                   echo 'Agencies: ' . "'";
+                   foreach ( $agencies_result AS $row ) 
+                   {
+                       if( $row1->province_en_name == $row->agency_province_name )
+                       {
+                           echo  '<div class="branch"><div><img src="' . $row->agency_url_logo . '" class="profile right"><ul class="info"><li class="cityname">' . $row->agency_city_name . '</li><li class="fullname">' . $row->agency_name . '</li></ul></div><div class="contact"><ul><li><span>مدیریت: </span> ' . $row->agency_full_name . '</li><li><span>' . $row->agency_tell . '</span>031-31234567</li><li><span>شماره همراه: </span>' . $row->agency_mobile . '</li><li><span>آدرس: </span>' . $row->agency_address . '</li></ul></div></div>';
+                       }
+                   }
+                   echo "'";
+                echo '},';           
+    }
+    echo '},';
+}
+
+?>

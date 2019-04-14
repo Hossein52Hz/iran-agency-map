6dad04046b6d07ac04cb764bfaea57f07b18b0dc
csmasoudi <csmasoudi@gmail.com>
2019-04-14 13:27:52 +0430

    fix generated x characters issue

--- includes/activation.php
+++ includes/activation.php
@@ -43,7 +43,7 @@ function imap_agency_install()
     //    Like this: PRIMARY KEY[space][space](id)
     // otherwise dbDelta will not work
     // --
-    // -- Table structure for table `wp_imap`
+    // -- Table structure for table wp_imap
     // --
     $sql = "CREATE TABLE " . $table_name . " (
         id int(11) NOT NULL AUTO_INCREMENT,
@@ -64,7 +64,7 @@ function imap_agency_install()
     dbDelta($sql);
 
     // --
-    // -- Table structure for table `wp_imap_province`
+    // -- Table structure for table wp_imap_province
     // --
     $imap_province_table_name = $wpdb->prefix . 'imap_province';
     $sql = "CREATE TABLE " . $imap_province_table_name . " (
@@ -75,57 +75,58 @@ function imap_agency_install()
     )ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
     
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql);
-
-    $insert_province_data = $wpdb->query("
-    INSERT INTO `wp_imap_province` (`province_en_name`, `province_fa_name`, `position_x`, `position_y`) VALUES
-    ('alborz', 'البرز', 240, 340),
-    ('ardabil', 'اردبیل', 100, 210),
-    ('bushehr', 'بوشهر', 640, 345),
-    ('caspian', 'دریای خزر', 0, 0),
-    ('chaharmahal-and-bakhtiari', 'چهارمحال و بختیاری', 480, 330),
-    ('east-azerbaijan', 'آذربایجان شرقی', 170, 80),
-    ('fars', 'فارس', 620, 430),
-    ('gilan', 'گیلان', 180, 280),
-    ('golestan', 'گلستان', 185, 520),
-    ('hamadan', 'همدان', 315, 225),
-    ('hormozgan', 'هرمزگان', 750, 600),
-    ('ilam', 'ایلام', 400, 130),
-    ('isfahan', 'اصفهان', 400, 400),
-    ('kerman', 'کرمان', 600, 650),
-    ('kermanshah', 'کرمانشاه', 330, 130),
-    ('khorasan-razavi', 'خراسان رضوی', 250, 730),
-    ('khuzestan', 'خوزستان', 500, 240),
-    ('kohgiluyeh-and-boyer-ahmad', 'کهگیلویه و بویراحمد', 550, 330),
-    ('kurdistan', 'کردستان', 260, 160),
-    ('lorestan', 'لرستان', 380, 210),
-    ('markazi', 'مرکزی', 330, 280),
-    ('mazandaran', 'مازندران', 230, 400),
-    ('north-khorasan', 'خراسان شمالی', 150, 630),
-    ('persian-gulf', 'خلیج فارس', 0, 0),
-    ('qazvin', 'قزوین', 240, 290),
-    ('qom', 'قم', 315, 345),
-    ('semnan', 'سمنان', 280, 530),
-    ('sistan-baluchestan', 'سیستان بلوچستان', 700, 850),
-    ('south-khorasan', 'خراسان جنوبی', 440, 750),
-    ('tehran', 'تهران', 270, 370),
-    ('west-azerbaijan', 'آذربایجان غربی', 130, 140),
-    ('yazd', 'یزد', 450, 570),
-    ('zanjan', 'زنجان', 210, 220)"
-    );
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($insert_province_data);
-    
-    // -- Indexes for table `wp_imap`
-    $set_fkey = $wpdb->query(" ALTER TABLE `wp_imap` ADD KEY `agency_province_name` (`agency_province_name`) ");
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($set_fkey);
-
-    // -- Indexes for table `wp_imap_province`
-    $set_pkey = $wpdb->query(" ALTER TABLE `wp_imap_province` ADD PRIMARY KEY (`province_en_name`) ");
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($set_pkey);
-
+    dbDelta($sql);  
+    if($wpdb->get_var( "SHOW TABLES LIKE 'wp_imap_province'" ) != 'wp_imap_province') {
+        $insert_province_data = $wpdb->query("
+        INSERT INTO wp_imap_province (province_en_name, province_fa_name, position_x, position_y) VALUES
+        ('alborz', 'البرز', 240, 340),
+        ('ardabil', 'اردبیل', 100, 210),
+        ('bushehr', 'بوشهر', 640, 345),
+        ('caspian', 'دریای خزر', 0, 0),
+        ('chaharmahal-and-bakhtiari', 'چهارمحال و بختیاری', 480, 330),
+        ('east-azerbaijan', 'آذربایجان شرقی', 170, 80),
+        ('fars', 'فارس', 620, 430),
+        ('gilan', 'گیلان', 180, 280),
+        ('golestan', 'گلستان', 185, 520),
+        ('hamadan', 'همدان', 315, 225),
+        ('hormozgan', 'هرمزگان', 750, 600),
+        ('ilam', 'ایلام', 400, 130),
+        ('isfahan', 'اصفهان', 400, 400),
+        ('kerman', 'کرمان', 600, 650),
+        ('kermanshah', 'کرمانشاه', 330, 130),
+        ('khorasan-razavi', 'خراسان رضوی', 250, 730),
+        ('khuzestan', 'خوزستان', 500, 240),
+        ('kohgiluyeh-and-boyer-ahmad', 'کهگیلویه و بویراحمد', 550, 330),
+        ('kurdistan', 'کردستان', 260, 160),
+        ('lorestan', 'لرستان', 380, 210),
+        ('markazi', 'مرکزی', 330, 280),
+        ('mazandaran', 'مازندران', 230, 400),
+        ('north-khorasan', 'خراسان شمالی', 150, 630),
+        ('persian-gulf', 'خلیج فارس', 0, 0),
+        ('qazvin', 'قزوین', 240, 290),
+        ('qom', 'قم', 315, 345),
+        ('semnan', 'سمنان', 280, 530),
+        ('sistan-baluchestan', 'سیستان بلوچستان', 700, 850),
+        ('south-khorasan', 'خراسان جنوبی', 440, 750),
+        ('tehran', 'تهران', 270, 370),
+        ('west-azerbaijan', 'آذربایجان غربی', 130, 140),
+        ('yazd', 'یزد', 450, 570),
+        ('zanjan', 'زنجان', 210, 220)"
+        );
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($insert_province_data);
+    }
+    if($wpdb->get_var( "SHOW TABLES LIKE 'wp_imap_province'" ) != 'wp_imap_province') {
+        // -- Indexes for table wp_imap
+        $set_fkey = $wpdb->query(" ALTER TABLE wp_imap ADD KEY agency_province_name (agency_province_name) ");
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($set_fkey);
+        
+        // -- Indexes for table wp_imap_province
+        $set_pkey = $wpdb->query(" ALTER TABLE wp_imap_province ADD PRIMARY KEY (province_en_name) ");
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($set_pkey);
+    }
 
 
     // save current database version for later use (on upgrade)

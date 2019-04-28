255f7b284aae68b460c3d2fa912afd9f70b30cb5
csmasoudi <csmasoudi@gmail.com>
2019-04-29 03:36:19 +0430

    fix x generation error by new method of insert

--- includes/activation.php
+++ includes/activation.php
@@ -41,6 +41,263 @@ function iran_agency_map_agency_install()
         
           require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
           dbDelta($iran_agency_map_table);  
+
+
+        function iam_insert_province_data($place_holders, $values) {
+            global $wpdb;
+            $province_info = $wpdb->prefix . 'iam_province_info';
+            $agencies_info = $wpdb->prefix . 'iam_agencies_info';
+            
+            $query = "INSERT INTO $province_info (province_en_name, province_fa_name, position_x, position_y) VALUES ";
+            $query .= implode( ', ', $place_holders );
+            $sql   = $wpdb->prepare( "$query ", $values );
+            $insert_data = $wpdb->query( $sql );
+            require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+            dbDelta($insert_data);
+        }
+        $data_to_be_inserted = array( array(
+            
+            'province_en_name'  => 'east-azerbaijan',
+            'province_fa_name'  => 'آذربایجان شرقی',
+            'position_x'        => 130,
+            'position_y'        => 140
+        ),
+        
+        array(
+            'province_en_name'  => 'west-azerbaijan',
+            'province_fa_name'  => 'آذربایجان غربی',
+            'position_x'        => 170,
+            'position_y'        => 80
+        ),
+        
+        array(
+            'province_en_name'  => 'ardabil',
+            'province_fa_name'  => 'اردبیل',
+            'position_x'        => 100,
+            'position_y'        => 210
+        ),
+        
+        array(
+            'province_en_name'  => 'isfahan',
+            'province_fa_name'  => 'اصفهان',
+            'position_x'        => 395,
+            'position_y'        => 398
+        ),
+        
+        array(
+            'province_en_name'  => 'alborz',
+            'province_fa_name'  => 'البرز',
+            'position_x'        => 240,
+            'position_y'        => 340
+        ),
+        
+        array(
+            'province_en_name'  => 'ilam',
+            'province_fa_name'  => 'ایلام',
+            'position_x'        => 385,
+            'position_y'        => 120
+        ),
+        
+        array(
+            'province_en_name'  => 'bushehr',
+            'province_fa_name'  => 'بوشهر',
+            'position_x'        => 640,
+            'position_y'        => 345
+        ),
+        
+        array(
+            'province_en_name'  => 'tehran',
+            'province_fa_name'  => 'تهران',
+            'position_x'        => 270,
+            'position_y'        => 370
+        ),
+        
+        array(
+            'province_en_name'  => 'chaharmahal-and-bakhtiari',
+            'province_fa_name'  => 'چهارمحال و بختیاری',
+            'position_x'        => 480,
+            'position_y'        => 330
+        ),
+        
+        array(
+            'province_en_name'  => 'south-khorasan',
+            'province_fa_name'  => 'خراسان جنوبی',
+            'position_x'        => 440,
+            'position_y'        => 750
+        ),
+        
+        array(
+            'province_en_name'  => 'khorasan-razavi',
+            'province_fa_name'  => 'خراسان رضوی',
+            'position_x'        => 250,
+            'position_y'        => 730
+        ),
+        
+        array(
+            'province_en_name'  => 'north-khorasan',
+            'province_fa_name'  => 'خراسان شمالی',
+            'position_x'        => 150,
+            'position_y'        => 630
+        ),
+        
+        array(
+            'province_en_name'  => 'khuzestan',
+            'province_fa_name'  => 'خوزستان',
+            'position_x'        => 500,
+            'position_y'        => 240
+        ),
+        
+        array(
+            'province_en_name'  => 'zanjan',
+            'province_fa_name'  => 'زنجان',
+            'position_x'        => 210,
+            'position_y'        => 220
+        ),
+        
+        array(
+            'province_en_name'  => 'semnan',
+            'province_fa_name'  => 'سمنان',
+            'position_x'        => 280,
+            'position_y'        => 530
+        ),
+        
+        array(
+            'province_en_name'  => 'sistan-baluchestan',
+            'province_fa_name'  => 'سیستان بلوچستان',
+            'position_x'        => 700,
+            'position_y'        => 850
+        ),
+        
+        array(
+            'province_en_name'  => 'fars',
+            'province_fa_name'  => 'فارس',
+            'position_x'        => 620,
+            'position_y'        => 430
+        ),
+        
+        array(
+            'province_en_name'  => 'qazvin',
+            'province_fa_name'  => 'قزوین',
+            'position_x'        => 240,
+            'position_y'        => 290
+        ),
+        
+        array(
+            'province_en_name'  => 'qom',
+            'province_fa_name'  => 'قم',
+            'position_x'        => 315,
+            'position_y'        => 345
+        ),
+        
+        array(
+            'province_en_name'  => 'kurdistan',
+            'province_fa_name'  => 'کردستان',
+            'position_x'        => 260,
+            'position_y'        => 160
+        ),
+        
+        array(
+            'province_en_name'  => 'kerman',
+            'province_fa_name'  => 'کرمان',
+            'position_x'        => 600,
+            'position_y'        => 650
+        ),
+        
+        array(
+            'province_en_name'  => 'kermanshah',
+            'province_fa_name'  => 'کرمانشاه',
+            'position_x'        => 330,
+            'position_y'        => 130
+        ),
+        
+        array(
+            'province_en_name'  => 'kohgiluyeh-and-boyer-ahmad',
+            'province_fa_name'  => 'کهگیلویه و بویراحمد',
+            'position_x'        => 550,
+            'position_y'        => 330
+        ),
+        
+        array(
+            'province_en_name'  => 'golestan',
+            'province_fa_name'  => 'گلستان',
+            'position_x'        => 185,
+            'position_y'        => 520
+        ),
+        
+        array(
+            'province_en_name'  => 'gilan',
+            'province_fa_name'  => 'گیلان',
+            'position_x'        => 180,
+            'position_y'        => 280
+        ),
+        
+        array(
+            'province_en_name'  => 'lorestan',
+            'province_fa_name'  => 'لرستان',
+            'position_x'        => 380,
+            'position_y'        => 210
+        ),
+        
+        array(
+            'province_en_name'  => 'mazandaran',
+            'province_fa_name'  => 'مازندران',
+            'position_x'        => 230,
+            'position_y'        => 400
+        ),
+        
+        array(
+            'province_en_name'  => 'markazi',
+            'province_fa_name'  => 'مرکزی',
+            'position_x'        => 330,
+            'position_y'        => 280
+        ),
+        
+        array(
+            'province_en_name'  => 'hormozgan',
+            'province_fa_name'  => 'هرمزگان',
+            'position_x'        => 750,
+            'position_y'        => 600
+        ),
+        
+        array(
+            'province_en_name'  => 'hamadan',
+            'province_fa_name'  => 'همدان',
+            'position_x'        => 315,
+            'position_y'        => 225
+        ),
+        
+        array(
+            'province_en_name'  => 'yazd',
+            'province_fa_name'  => 'یزد',
+            'position_x'        => 450,
+            'position_y'        => 570
+        ),
+        
+        array(
+            'province_en_name'  => 'persian-gulf',
+            'province_fa_name'  => 'خلیج فارس',
+            'position_x'        => 0,
+            'position_y'        => 0
+        ),
+        
+        array(
+            'province_en_name' => 'caspian',
+            'province_fa_name' => 'دریای خزر',
+            'position_x'       => 0,
+            'position_y'       => 0
+        ));
+        
+        
+        $values = $place_holders = array();
+        
+        if(count($data_to_be_inserted) > 0) {
+            foreach($data_to_be_inserted as $data) {
+                array_push( $values, $data['province_en_name'], $data['province_fa_name'], $data['position_x'], $data['position_y']);
+                $place_holders[] = "( '%s', '%s', '%d', '%d')";
+            }
+        
+            iam_insert_province_data( $place_holders, $values );
+        }
     }
 
     // --
@@ -64,44 +321,6 @@ function iran_agency_map_agency_install()
 
         require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
         dbDelta($iran_agency_map_create_table);
-
-        $iran_agency_map_insert_province_data = $wpdb->query($wpdb->prepare("INSERT INTO `".$province_info."` (province_en_name, province_fa_name, position_x, position_y) VALUES
-        ('east-azerbaijan', 'آذربایجان شرقی', 130, 140),
-        ('west-azerbaijan', 'آذربایجان غربی', 170, 80),
-        ('ardabil', 'اردبیل', 100, 210),
-        ('isfahan', 'اصفهان', 395, 398),
-        ('alborz', 'البرز', 240, 340),
-        ('ilam', 'ایلام', 385, 120),
-        ('bushehr', 'بوشهر', 640, 345),
-        ('tehran', 'تهران', 270, 370),
-        ('chaharmahal-and-bakhtiari', 'چهارمحال و بختیاری', 480, 330),
-        ('south-khorasan', 'خراسان جنوبی', 440, 750),
-        ('khorasan-razavi', 'خراسان رضوی', 250, 730),
-        ('north-khorasan', 'خراسان شمالی', 150, 630),
-        ('khuzestan', 'خوزستان', 500, 240),
-        ('zanjan', 'زنجان', 210, 220),
-        ('semnan', 'سمنان', 280, 530),
-        ('sistan-baluchestan', 'سیستان بلوچستان', 700, 850),
-        ('fars', 'فارس', 620, 430),
-        ('qazvin', 'قزوین', 240, 290),
-        ('qom', 'قم', 315, 345),
-        ('kurdistan', 'کردستان', 260, 160),
-        ('kerman', 'کرمان', 600, 650),
-        ('kermanshah', 'کرمانشاه', 330, 130),
-        ('kohgiluyeh-and-boyer-ahmad', 'کهگیلویه و بویراحمد', 550, 330),
-        ('golestan', 'گلستان', 185, 520),
-        ('gilan', 'گیلان', 180, 280),
-        ('lorestan', 'لرستان', 380, 210),
-        ('mazandaran', 'مازندران', 230, 400),
-        ('markazi', 'مرکزی', 330, 280),
-        ('hormozgan', 'هرمزگان', 750, 600),
-        ('hamadan', 'همدان', 315, 225),
-        ('yazd', 'یزد', 450, 570),
-        ('persian-gulf', 'خلیج فارس', 0, 0),
-        ('caspian', 'دریای خزر', 0, 0)"
-        ));
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        dbDelta($iran_agency_map_insert_province_data);
         
     }
 
